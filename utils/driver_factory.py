fb0d72bff5debc0e0486f0f8e133a30599b0880c
Rakesh6290 <rakerakeshrock@gmail.com>
2026-05-09 18:11:15 +0530

    Added browser drivers for Jenkins execution

--- utils/driver_factory.py
+++ utils/driver_factory.py
@@ -1,17 +1,25 @@
 from selenium import webdriver
+
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.firefox.service import Service as FirefoxService
 from selenium.webdriver.edge.service import Service as EdgeService
+
 import os
 
 
 def get_driver(browser="chrome"):
 
-    project_root = os.getcwd()
+    base_path = os.path.dirname(
+        os.path.dirname(__file__)
+    )
 
     if browser.lower() == "chrome":
 
-        driver_path = os.path.join(project_root, "drivers", "chromedriver.exe")
+        driver_path = os.path.join(
+            base_path,
+            "drivers",
+            "chromedriver.exe"
+        )
 
         driver = webdriver.Chrome(
             service=ChromeService(driver_path)
@@ -19,7 +27,11 @@ def get_driver(browser="chrome"):
 
     elif browser.lower() == "firefox":
 
-        driver_path = os.path.join(project_root, "drivers", "geckodriver.exe")
+        driver_path = os.path.join(
+            base_path,
+            "drivers",
+            "geckodriver.exe"
+        )
 
         driver = webdriver.Firefox(
             service=FirefoxService(driver_path)
@@ -27,14 +39,22 @@ def get_driver(browser="chrome"):
 
     elif browser.lower() == "edge":
 
-        driver_path = os.path.join(project_root, "drivers", "msedgedriver.exe")
+        driver_path = os.path.join(
+            base_path,
+            "drivers",
+            "msedgedriver.exe"
+        )
 
         driver = webdriver.Edge(
             service=EdgeService(driver_path)
         )
 
     else:
-        raise Exception(f"Browser '{browser}' not supported")
+
+        raise Exception(
+            f"Browser '{browser}' not supported."
+        )
 
     driver.maximize_window()
+
     return driver
