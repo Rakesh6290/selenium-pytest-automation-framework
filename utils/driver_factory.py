f20d7c7b6925115447ae97a47caf6618d4bf790c
Rakesh6290 <rakerakeshrock@gmail.com>
2026-05-09 18:35:16 +0530

    Configured local drivers for Jenkins execution

--- utils/driver_factory.py
+++ utils/driver_factory.py
@@ -4,49 +4,31 @@ from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.firefox.service import Service as FirefoxService
 from selenium.webdriver.edge.service import Service as EdgeService
 
-import os
-
 
 def get_driver(browser="chrome"):
 
-    base_path = os.path.dirname(
-        os.path.dirname(__file__)
-    )
-
     if browser.lower() == "chrome":
 
-        driver_path = os.path.join(
-            base_path,
-            "drivers",
-            "chromedriver.exe"
-        )
-
         driver = webdriver.Chrome(
-            service=ChromeService(driver_path)
+            service=ChromeService(
+                r"C:\drivers\chromedriver.exe"
+            )
         )
 
     elif browser.lower() == "firefox":
 
-        driver_path = os.path.join(
-            base_path,
-            "drivers",
-            "geckodriver.exe"
-        )
-
         driver = webdriver.Firefox(
-            service=FirefoxService(driver_path)
+            service=FirefoxService(
+                r"C:\drivers\geckodriver.exe"
+            )
         )
 
     elif browser.lower() == "edge":
 
-        driver_path = os.path.join(
-            base_path,
-            "drivers",
-            "msedgedriver.exe"
-        )
-
         driver = webdriver.Edge(
-            service=EdgeService(driver_path)
+            service=EdgeService(
+                r"C:\drivers\msedgedriver.exe"
+            )
         )
 
     else:
